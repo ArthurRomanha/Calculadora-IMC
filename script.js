781e443d4f383a141c4a89e102b09a4e5b342fe6
Arthur Romanha <126591597+ArthurRomanha@users.noreply.github.com>
2025-02-28 10:54:43 -0300

    1.0

--- script.js
+++ script.js
@@ -14,8 +14,8 @@ const calcular = () => {
     } else if(resultado > 30 && resultado < 34.99){
         divResultado.textContent = `Olá ${nome}, seu IMC é de: ${resultado} e você se enquadra em: Obesidade Grau I`;
     }else if(resultado > 35 && resultado < 39.99){
-        divResultado.textContent = `Olá ${nome}, seu IMC é de: ${resultado} e você se enquadra em: Obesidade Grau I`;
+        divResultado.textContent = `Olá ${nome}, seu IMC é de: ${resultado} e você se enquadra em: Obesidade Grau II`;
     }else if(resultado >= 40){
-        divResultado.textContent = `Olá ${nome}, seu IMC é de: ${resultado} e você se enquadra em: Obesidade Grau I`;
+        divResultado.textContent = `Olá ${nome}, seu IMC é de: ${resultado} e você se enquadra em: Obesidade Grau III`;
     }
 }
